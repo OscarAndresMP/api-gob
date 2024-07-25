52b1725722709412e0c46382dbaae2879ca542ea
OscarAndresMP <oscarandresmoralesponce@gmail.com>
2024-07-25 13:05:04 -0600

    api 1.1

--- modulos/conexion.php
+++ modulos/conexion.php
@@ -3,18 +3,18 @@
     class Conexion{
 
         static public function conectar(){
-        session_start();
+        /*session_start();
             $DB_HOST = $_ENV["DB_HOST"];
             $DB_USER = $_ENV["DB_USER"];
             $DB_PASSWORD= $_ENV["DB_PASSWORD"];
             $DB_NAME = $_ENV["DB_NAME"];
-            $DB_PORT = $_ENV["DB_PORT"];
+            $DB_PORT = $_ENV["DB_PORT"];*/
 
-        /* $DB_HOST = "viaduct.proxy.rlwy.net";
+        $DB_HOST = "viaduct.proxy.rlwy.net";
             $DB_USER = "postgres";
             $DB_PASSWORD= "iZNlCgLnfLhwIosyWHVhaRBmBlVvRrxI";
             $DB_NAME = "railway";
-            $DB_PORT ="22202";*/
+            $DB_PORT ="22202";
 
             $link = new PDO("pgsql:host=$DB_HOST;port=$DB_PORT;dbname=$DB_NAME;user=$DB_USER;password=$DB_PASSWORD");
             //$link=pg_connect("host=$DB_HOST port=$DB_PORT dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD");
--- users.sql
+++
@@ -1,9 +0,0 @@
-create table users(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-etname VARCHAR(50), etnamefather VARCHAR(50), 
-etnamemother VARCHAR(50), etemail VARCHAR(50), 
-etpassword VARCHAR(50), etcel VARCHAR(50), 
-etbirthday VARCHAR(50), acvdependence VARCHAR(50), 
-acvsex VARCHAR(50), created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP);
-
-INSERT INTO users (etname, etnamefather, etnamemother, etemail, etpassword, etcel, etbirthday, acvdependence, acvsex) 
-VALUES ('Jose', 'Perez', 'Perez', 'JosePerez@gmail.com', '5571758674', '5571758674', '15/04/1990', 'dependence', 'masculino');
